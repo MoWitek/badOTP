988f363ac066e16af8684db7c77e41a2032db912
MoWitek <75223550+MoWitek@users.noreply.github.com>
2021-02-23 14:05:40 +0100

    Update __init__.py

--- V1/python package/__init__.py
+++ V1/python package/__init__.py
@@ -1,1 +1,4 @@
-None
+from badOTP.scramble_file import scramble_file
+from badOTP.scramble_str import scramble_str
+from badOTP.secure import secure
+from badOTP.calculate_security import calculate_security
